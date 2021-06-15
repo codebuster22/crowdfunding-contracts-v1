ca7a15bcd4a0df0091cdb447b94f6290e3ac1887
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-06-15 13:06:16 +0530

    added new test for addProposal()

--- test/dao.js
+++ test/dao.js
@@ -1,11 +1,12 @@
 const DAO = artifacts.require('DAO');
 const { expect } = require('chai');
-const {BN} = web3.utils;
+const { BN, toWei } = web3.utils;
 
 contract('DAO', async (accounts) => {
     let [deployer, owner2] = accounts;
     let dao;
-    context('# Deploy new DAO',() => {
+    let options;
+    context('# Deploy new DAO', async () => {
         before("!! Deploy dao for testing", async () => {
             dao = await DAO.new();
         })
@@ -24,6 +25,29 @@ contract('DAO', async (accounts) => {
         });
         it(">> totalOwners should increase by 1", async () => {
             expect((await dao.totalOwners()).toString()).to.equal('2');
+        });
+    });
+    context('# Add new proposal', async () => {
+        before("!! Create argument options for new proposal", async () => {
+            options = [
+                "Fundraiser for Blockchain batch-2",
+                '0x',
+                toWei('100'),
+                toWei('10'),
+                30
+            ];
+        });
+        it(">> should add new proposal", async () => {
+            await dao.addProposal(...options);
+            const proposal = await dao.proposals(0);
+            expect(proposal['0']).to.equal(options[0]);
+            expect(proposal['1']).to.equal(null);
+            expect(proposal['2']).to.equal(deployer);
+            expect(proposal['3'].toString()).to.equal(options[2].toString());
+            expect(proposal['4'].toString()).to.equal(options[3].toString());
+            expect(proposal['5'].toString()).to.equal(options[4].toString());
+            expect(proposal['6'].toString()).to.equal('0');
+            expect((await dao.campaignManagers(deployer)).toString()).to.equal('0')
         });
     });
 });
